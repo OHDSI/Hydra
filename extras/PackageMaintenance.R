7b0092fd8f7d9e287bbb9f18a658c8a48970c6c7
schuemie <schuemie@ohdsi.org>
2018-10-01 16:48:13 +0200

    Adding PLP and PLE skeletons

--- extras/PackageMaintenance.R
+++ extras/PackageMaintenance.R
@@ -30,3 +30,36 @@ rmarkdown::render("vignettes/WritingHydraConfigs.Rmd",
                   rmarkdown::pdf_document(latex_engine = "pdflatex",
                                           toc = TRUE,
                                           number_sections = TRUE))
+
+# Import comparative effectiveness study skeleton
+skeletonSource <- "C:/Users/mschuemi/Git/SkeletonComparativeEffectStudy"
+skeletonName <- "ComparativeEffectStudy_v0.0.1.zip"
+tempFolder <- "c:/temp/skeleton"
+
+unlink(tempFolder, recursive = TRUE, force = TRUE)
+dir.create(tempFolder, recursive = TRUE)
+file.copy(skeletonSource, tempFolder, recursive = TRUE)
+skeletonFolder <- file.path(tempFolder, "SkeletonComparativeEffectStudy")
+unlink(file.path(skeletonFolder, "readme.md"))
+file.rename(file.path(skeletonFolder, "studyReadme.md"), file.path(skeletonFolder, "readme.md"))
+unlink(file.path(skeletonFolder, "vignettes"), recursive = TRUE)
+unlink(file.path(skeletonFolder, ".git"), recursive = TRUE, force = TRUE)
+unlink(file.path(skeletonFolder, ".Rproj.user"), recursive = TRUE)
+unlink(file.path(skeletonFolder, ".Rhistory"), recursive = TRUE)
+unlink(file.path(skeletonFolder, "inst", "shiny", "EvidenceExplorer", ".Rproj.user"), recursive = TRUE)
+unlink(file.path(skeletonFolder, "inst", "shiny", "EvidenceExplorer", ".Rhistory"), recursive = TRUE)
+unlink(file.path(skeletonFolder, "inst", "doc"), recursive = TRUE)
+unlink(file.path(skeletonFolder, "extras", "CreateStudyAnalysisDetails.R"))
+files <- list.files(file.path(skeletonFolder, "inst", "cohorts"), ".json", full.names = TRUE)
+unlink(files)
+files <- gsub(".json$", ".sql", gsub("cohorts", "sql/sql_server", files))
+unlink(files)
+files <- list.files(file.path(skeletonFolder, "inst", "settings"), full.names = TRUE)
+unlink(files)
+oldWd <- setwd(skeletonFolder)
+DatabaseConnector::createZipFile(zipFile = file.path(tempFolder, skeletonName), 
+                                 skeletonFolder)
+setwd(oldWd)
+file.rename(file.path(tempFolder, skeletonName), 
+            file.path("inst", "skeletons", skeletonName))
+unlink(tempFolder, recursive = TRUE)
