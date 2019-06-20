dfd17f61b09fdd432dc72c21e664f19ba4f29492
schuemie <schuemie@ohdsi.org>
2019-06-20 05:35:40 -0400

    Adding testing script for PLE

---
+++ extras/TestPle.R
@@ -0,0 +1,54 @@
+# Hydrate skeleton with example specifications ---------------------------------
+library(Hydra)
+specifications <- loadSpecifications("extras/ExamplePleSpecs.json")
+packageFolder <- "c:/temp/hydraOutput"
+unlink(packageFolder, recursive = TRUE)
+hydrate(specifications = specifications, outputFolder = packageFolder)
+
+# Build and install hydrated package -------------------------------------------
+devtools::install(packageFolder, upgrade = "never")
+
+# Run the package --------------------------------------------------------------
+library(pleTestPackage)
+options(fftempdir = "s:/FFtemp")
+maxCores <- parallel::detectCores()
+outputFolder <- "s:/pleTestPackage"
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "pdw",
+                                                                server = Sys.getenv("PDW_SERVER"),
+                                                                user = NULL,
+                                                                password = NULL,
+                                                                port = Sys.getenv("PDW_PORT"))
+cdmDatabaseSchema <- "cdm_truven_mdcd_v699.dbo"
+cohortDatabaseSchema <- "scratch.dbo"
+cohortTable <- "mschuemi_skeleton"
+databaseId <- "Synpuf"
+databaseName <- "Medicare Claims Synthetic Public Use Files (SynPUFs)"
+databaseDescription <- "Medicare Claims Synthetic Public Use Files (SynPUFs) were created to allow interested parties to gain familiarity using Medicare claims data while protecting beneficiary privacy. These files are intended to promote development of software and applications that utilize files in this format, train researchers on the use and complexities of Centers for Medicare and Medicaid Services (CMS) claims, and support safe data mining innovations. The SynPUFs were created by combining randomized information from multiple unique beneficiaries and changing variable values. This randomization and combining of beneficiary information ensures privacy of health information."
+oracleTempSchema <- NULL
+
+unlink(outputFolder, recursive = TRUE)
+
+execute(connectionDetails = connectionDetails,
+        cdmDatabaseSchema = cdmDatabaseSchema,
+        cohortDatabaseSchema = cohortDatabaseSchema,
+        cohortTable = cohortTable,
+        oracleTempSchema = oracleTempSchema,
+        outputFolder = outputFolder,
+        databaseId = databaseId,
+        databaseName = databaseName,
+        databaseDescription = databaseDescription,
+        createCohorts = TRUE,
+        synthesizePositiveControls = TRUE,
+        runAnalyses = TRUE,
+        runDiagnostics = TRUE,
+        packageResults = TRUE,
+        maxCores = maxCores)
+
+resultsZipFile <- file.path(outputFolder, "export", paste0("Results", databaseId, ".zip"))
+dataFolder <- file.path(outputFolder, "shinyData")
+
+prepareForEvidenceExplorer(resultsZipFile = resultsZipFile, dataFolder = dataFolder)
+
+launchEvidenceExplorer(dataFolder = dataFolder, blind = TRUE, launch.browser = FALSE)
+
+launchEvidenceExplorer(dataFolder = dataFolder, blind = FALSE, launch.browser = FALSE)
