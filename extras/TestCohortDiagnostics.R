5bc65d8583f9ff07e24b8a76c7807be009533e02
Gowtham Rao <gowthamrao@gmail.com>
2021-04-11 17:55:17 -0400

    Updated based on feedback

--- extras/TestCohortDiagnostics.R
+++ extras/TestCohortDiagnostics.R
@@ -1,57 +1,68 @@
-# Hydrate skeleton with example specifications --------------------------------- 
-id <- 1
-version <- "v0.1.0"
-name <- "Study of some cohorts of interest"
-packageName <- "eunomiaExamplePackage"
-skeletonVersion <- "v0.0.1"
-createdBy <- "rao@ohdsi.org"
-createdDate <- Sys.Date()
-modifiedBy <- "rao@ohdsi.org"
-modifiedDate <- NA
-skeletonType <- "CohortDiagnosticsStudy"
-organizationName <- "OHDSI"
-description <- "Cohort diagnostics on selected set of cohorts."
+rootFolder <- "D:/temp"
+
+########### Code to generate ExampleCohortDiagnosticsSpecs.json #####################
+# # Hydrate skeleton with example specifications --------------------------------- 
+# id <- 1
+# version <- "v0.1.0"
+# name <- "Study of some cohorts of interest"
+# packageName <- "eunomiaExamplePackage"
+# skeletonVersion <- "v0.0.1"
+# createdBy <- "rao@ohdsi.org"
+# createdDate <- Sys.Date()
+# modifiedBy <- "rao@ohdsi.org"
+# modifiedDate <- NA
+# skeletonType <- "CohortDiagnosticsStudy"
+# organizationName <- "OHDSI"
+# description <- "Cohort diagnostics on selected set of cohorts."
+# 
+# 
+# library(magrittr)
+# # Set up
+# baseUrl <- "http://api.ohdsi.org:8080/WebAPI"
+# cohortIds <- c(82,1776966)
+# 
+# # compile them into a data table
+# cohortDefinitionsArray <- list()
+# for (i in (1:length(cohortIds))) {
+#         cohortDefinition <-
+#                 ROhdsiWebApi::getCohortDefinition(cohortId = cohortIds[[i]], baseUrl = baseUrl)
+#         cohortDefinitionsArray[[i]] <- list(
+#                 id = cohortDefinition$id,
+#                 createdDate = cohortDefinition$createdDate,
+#                 name = stringr::str_trim(stringr::str_squish(cohortDefinition$name)),
+#                 expression = cohortDefinition$expression
+#         )
+# }
+# 
+# specifications <- list(id = id,
+#                        version = version,
+#                        name = name,
+#                        packageName = packageName,
+#                        skeletonVersin = skeletonVersion,
+#                        createdBy = createdBy,
+#                        createdDate = createdDate,
+#                        modifiedBy = modifiedBy,
+#                        modifiedDate = modifiedDate,
+#                        skeletontype = skeletonType,
+#                        organizationName = organizationName,
+#                        description = description,
+#                        cohortDefinitions = cohortDefinitionsArray)
+# 
+# jsonFileName <- paste0(file.path(rstudioapi::getActiveProject(), "extras/ExampleCohortDiagnosticsSpecs.json"))
+# write(x = specifications %>% RJSONIO::toJSON(pretty = TRUE), file = jsonFileName)
 
 
+##############################################################
+##############################################################
+#######               Build package              #############
+##############################################################
+##############################################################
+##############################################################
 
-library(magrittr)
-# Set up
-baseUrl <- "http://api.ohdsi.org:8080/WebAPI"
-cohortIds <- c(1776966)
-
-# compile them into a data table
-cohortDefinitionsArray <- list()
-for (i in (1:length(cohortIds))) {
-        cohortDefinition <-
-                ROhdsiWebApi::getCohortDefinition(cohortId = cohortIds[[i]], baseUrl = baseUrl)
-        cohortDefinitionsArray[[i]] <- list(
-                id = cohortDefinition$id,
-                createdDate = cohortDefinition$createdDate,
-                name = stringr::str_trim(stringr::str_squish(cohortDefinition$name)),
-                expression = cohortDefinition$expression
-        )
-}
-
-specifications <- list(id = id,
-                       version = version,
-                       name = name,
-                       packageName = packageName,
-                       skeletonVersin = skeletonVersion,
-                       createdBy = createdBy,
-                       createdDate = createdDate,
-                       modifiedBy = modifiedBy,
-                       modifiedDate = modifiedDate,
-                       skeletontype = skeletonType,
-                       organizationName = organizationName,
-                       description = description,
-                       cohortDefinitions = cohortDefinitionsArray)
-
-jsonFileName <- paste0(tempfile(), ".json")
-write(x = specifications %>% rjson::toJSON(), file = jsonFileName)
+#### Code that uses the ExampleCohortDiagnosticsSpecs in Hydra to build package
+jsonFileName <- paste0(file.path(rstudioapi::getActiveProject(), "extras/ExampleCohortDiagnosticsSpecs.json"))
 hydraSpecificationFromFile <- Hydra::loadSpecifications(fileName = jsonFileName)
-unlink(x = jsonFileName, force = TRUE)
-
-packageFolder <- "c:/temp/hydraOutput/CohortDiagnostics"
+packageFolder <- file.path(rootFolder, "hydraOutput/CohortDiagnostics")
 unlink(x = packageFolder, recursive = TRUE)
 Hydra::hydrate(specifications = hydraSpecificationFromFile,
                outputFolder = packageFolder, 
@@ -65,25 +76,25 @@ Hydra::hydrate(specifications = hydraSpecificationFromFile,
 
 ##############################################################
 ##############################################################
+######       Build and install package           #############
 ##############################################################
 ##############################################################
 ##############################################################
-
-
 
 # Build and install hydrated package -------------------------------------------
 devtools::install(packageFolder, upgrade = "never")
 
 # Run the package --------------------------------------------------------------
-library(pleTestPackage)
 maxCores <- parallel::detectCores()
-outputFolder <- "s:/CohortDiagnosticsTestPackage"
+outputFolder <- file.path(rootFolder, "testResults")
 connectionDetails <- Eunomia::getEunomiaConnectionDetails()
 cdmDatabaseSchema <- "main"
 cohortDatabaseSchema <- "main"
 cohortTable <- "Eunomia"
 
 unlink(outputFolder, recursive = TRUE)
+
+library("eunomiaExamplePackage")
 
 runCohortDiagnostics(
         packageName = packageName,
