a8faa84c425bba3fbd2015a06f31035feab7deea
schuemie <schuemie@ohdsi.org>
2021-06-02 08:51:24 -0400

    Working on testing scripts

--- extras/TestCohortDiagnostics.R
+++ extras/TestCohortDiagnostics.R
@@ -1,141 +1,85 @@
-rootFolder <- "D:/temp"
-
-########## Code to generate ExampleCohortDiagnosticsSpecs.json #####################
-# Hydrate skeleton with example specifications ---------------------------------
-
-# id <- 1
-# version <- "v0.1.0"
-# name <- "Study of some cohorts of interest"
-# packageName <- "eunomiaExamplePackage"
-# skeletonVersion <- "v0.0.1"
-# createdBy <- "rao@ohdsi.org"
-# createdDate <- Sys.Date()
-# modifiedBy <- "rao@ohdsi.org"
-# modifiedDate <- NA
-# skeletonType <- "CohortDiagnosticsStudy"
-# organizationName <- "OHDSI"
-# description <- "Cohort diagnostics on selected set of cohorts."
+# Need to restart R (and RStudio if working in RStudio) at some point when restoring a renv lock file
+# because some packages like rJava and rlang cannot be updated in a session.
 # 
-# 
-# library(magrittr)
-# # Set up
-# baseUrl <- Sys.getenv("BaseUrl")
-# webApiCohorts <- ROhdsiWebApi::getCohortDefinitionsMetaData(baseUrl = baseUrl)
-# studyCohorts <-  webApiCohorts %>% 
-#         dplyr::filter(.data$id %in% c(18345,18346,14906,18351,18347,18348,14907,
-#                                       18349,18350,18352,17493,17492,14909,18342,
-#                                       17693,17692,17695,17694,17720, 
-#                                       21402
-#         ))
-# 
-# # compile them into a data table
-# cohortDefinitionsArray <- list()
-# for (i in (1:nrow(studyCohorts))) {
-#         cohortDefinition <-
-#                 ROhdsiWebApi::getCohortDefinition(cohortId = studyCohorts$id[[i]], 
-#                                                   baseUrl = baseUrl)
-#         cohortDefinitionsArray[[i]] <- list(
-#                 id = studyCohorts$id[[i]],
-#                 createdDate = studyCohorts$createdDate[[i]],
-#                 modifiedDate = studyCohorts$createdDate[[i]],
-#                 logicDescription = studyCohorts$description[[i]],
-#                 name = stringr::str_trim(stringr::str_squish(cohortDefinition$name)),
-#                 expression = cohortDefinition$expression
-#         )
-# }
-# 
-# specifications <- list(id = id,
-#                        version = version,
-#                        name = name,
-#                        packageName = packageName,
-#                        skeletonVersin = skeletonVersion,
-#                        createdBy = createdBy,
-#                        createdDate = createdDate,
-#                        modifiedBy = modifiedBy,
-#                        modifiedDate = modifiedDate,
-#                        skeletontype = skeletonType,
-#                        organizationName = organizationName,
-#                        description = description,
-#                        cohortDefinitions = cohortDefinitionsArray)
-# 
-# jsonFileName <- paste0(file.path(rstudioapi::getActiveProject(), "extras/ExampleCohortDiagnosticsSpecs.json"))
-# write(x = specifications %>% RJSONIO::toJSON(pretty = TRUE), file = jsonFileName)
+# As a demonstration of how to do this when not in interactive mode, using a 
+# ParallelLogger with single thread here, as well as renv::run. Could also kick off a new R session
+# from the command prompt.
+
+# Hydrate skeleton with example specifications -----------------------------------
+library(Hydra)
+specifications <- loadSpecifications("extras/ExampleCohortDiagnosticsSpecs.json")
+packageFolder <- "s:/temp/hydraCdOutput"
+unlink(packageFolder, recursive = TRUE)
+hydrate(specifications = specifications, outputFolder = packageFolder)
+
+# Build and install hydrated package with renv library ---------------------------
+buildPackageWithRenvLibrary <- function(packageFolder) {
+        renv::load(packageFolder)
+        renv::restore(prompt = FALSE)
+        
+        # To prevent renv::install from fetching dependencies, set dependency.fields 
+        # to LinkingTo, since study packages don't have that section:
+        old <- renv::settings$package.dependency.fields() 
+        renv::settings$package.dependency.fields(value = c("LinkingTo")) 
+        
+        renv::install(packageFolder, type = "source")
+        
+        renv::settings$package.dependency.fields(value = old)
+        
+        return(TRUE)
+}
+newSession <- ParallelLogger::makeCluster(numberOfThreads = 1, singleThreadToMain = FALSE)
+ParallelLogger::clusterApply(cluster = newSession, fun = buildPackageWithRenvLibrary, x = packageFolder)
+ParallelLogger::stopCluster(newSession)
+
+# Run the package ------------------------------------------------------------
+script <- "
+        setwd(packageFolder)
+        library(eunomiaExamplePackage)
+        options(andromedaTempFolder = 's:/andromedaTemp')
+        
+        maxCores <- parallel::detectCores()
+        outputFolder <- 's:/cdTestPackage'
+        unlink(outputFolder, recursive = TRUE)
+        connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = 'pdw',
+                                                                        server = Sys.getenv('PDW_SERVER'),
+                                                                        user = NULL,
+                                                                        password = NULL,
+                                                                        port = Sys.getenv('PDW_PORT'))
+        cdmDatabaseSchema <- 'CDM_IBM_MDCD_V1153.dbo'
+        cohortDatabaseSchema <- 'scratch.dbo'
+        cohortTable <- 'mschuemi_skeleton'
+        options(sqlRenderTempEmulationSchema = NULL)
+        databaseId <- 'Synpuf'
+        databaseName <- 'Medicare Claims Synthetic Public Use Files (SynPUFs)'
+        databaseDescription <- 'Medicare Claims Synthetic Public Use Files (SynPUFs) were created to allow interested parties to gain familiarity using Medicare claims data while protecting beneficiary privacy. These files are intended to promote development of software and applications that utilize files in this format, train researchers on the use and complexities of Centers for Medicare and Medicaid Services (CMS) claims, and support safe data mining innovations. The SynPUFs were created by combining randomized information from multiple unique beneficiaries and changing variable values. This randomization and combining of beneficiary information ensures privacy of health information.'
+        
+        unlink(outputFolder, recursive = TRUE)
+        
+        execute(connectionDetails = connectionDetails,
+                cdmDatabaseSchema = cdmDatabaseSchema,
+                cohortDatabaseSchema = cohortDatabaseSchema,
+                cohortTable = cohortTable,
+                verifyDependencies = TRUE,
+                outputFolder = outputFolder,
+                databaseId = databaseId,
+                databaseName = databaseId,
+                databaseDescription = databaseId)
+"
+script <- gsub("packageFolder", sprintf("\"%s\"", packageFolder), script)
+tempScriptFile <- tempfile(fileext = ".R")
+sink(tempScriptFile)
+cat(script)
+sink()
+
+renv::run(script = tempScriptFile,
+          name = "Study package",
+          project = packageFolder)
 
 
-##############################################################
-##############################################################
-#######               Build package              #############
-##############################################################
-##############################################################
-##############################################################
+# Not part of study execution: View results in Shiny app -----------------------
+renv::load(packageFolder)
 
-#### Code that uses the ExampleCohortDiagnosticsSpecs in Hydra to build package
-jsonFileName <- paste0(file.path(rstudioapi::getActiveProject(), "extras/ExampleCohortDiagnosticsSpecs.json"))
-hydraSpecificationFromFile <- Hydra::loadSpecifications(fileName = jsonFileName)
-packageFolder <- file.path(rootFolder, "hydraOutput/CohortDiagnostics")
-unlink(x = packageFolder, recursive = TRUE)
-Hydra::hydrate(specifications = hydraSpecificationFromFile,
-               outputFolder = packageFolder, 
-               skeletonFileName = system.file("skeletons", 
-                                              "CohortDiagnosticsStudy_v0.0.1.zip", 
-                                              package = "Hydra")
-)
+CohortDiagnostics::preMergeDiagnosticsFiles(dataFolder = "s:/cdTestPackage")
 
-
-
-
-##############################################################
-##############################################################
-######       Build and install package           #############
-##############################################################
-##############################################################
-##############################################################
-
-# Build and install hydrated package -------------------------------------------
-devtools::install(packageFolder, upgrade = "never")
-
-# Run the package --------------------------------------------------------------
-maxCores <- parallel::detectCores()
-outputFolder <- file.path(rootFolder, "testResults")
-connectionDetails <- Eunomia::getEunomiaConnectionDetails()
-cdmDatabaseSchema <- "main"
-cohortDatabaseSchema <- "main"
-cohortTable <- "EunomiaCohortTable"
-
-unlink(outputFolder, recursive = TRUE)
-
-library("eunomiaExamplePackage")
-
-connectionDetails <- Eunomia::getEunomiaConnectionDetails()
-databaseId = "Eunomia"
-cdmDatabaseSchema <- "main"
-cohortDatabaseSchema <- "main"
-cohortTable <- "cohort"
-oracleTempSchema <- NULL
-
-runCohortDiagnostics(
-        connectionDetails = connectionDetails,
-        cdmDatabaseSchema = cdmDatabaseSchema,
-        cohortDatabaseSchema = cohortDatabaseSchema,
-        cohortTable = cohortTable,
-        outputFolder = outputFolder,
-        databaseId = databaseId,
-        databaseName = databaseId,
-        databaseDescription = databaseId,
-        runCohortCharacterization = TRUE,
-        runCohortOverlap = TRUE,
-        runOrphanConcepts = TRUE,
-        runVisitContext = TRUE,
-        runIncludedSourceConcepts = TRUE,
-        runTimeDistributions = TRUE,
-        runTemporalCohortCharacterization = TRUE,
-        runBreakdownIndexEvents = TRUE,
-        runInclusionStatistics = TRUE,
-        runIncidenceRates = TRUE,
-        createCohorts = TRUE,
-        minCellCount = 0
-)
-
-CohortDiagnostics::preMergeDiagnosticsFiles(dataFolder = outputFolder)
-
-CohortDiagnostics::launchDiagnosticsExplorer(dataFolder = outputFolder)
+CohortDiagnostics::launchDiagnosticsExplorer(dataFolder = "s:/cdTestPackage")
--- extras/TestPle.R
+++ extras/TestPle.R
@@ -1,53 +1,67 @@
+# setwd( "C:/Users/mschuemi/git/Hydra")
+
 # Hydrate skeleton with example specifications ---------------------------------
 library(Hydra)
 specifications <- loadSpecifications("extras/ExamplePleSpecs.json")
-packageFolder <- "c:/temp/hydraOutput"
+packageFolder <- "s:/temp/hydraOutput"
 unlink(packageFolder, recursive = TRUE)
 hydrate(specifications = specifications, outputFolder = packageFolder)
 
 # Build and install hydrated package -------------------------------------------
-devtools::install(packageFolder, upgrade = "never")
+renv::load(packageFolder)
+renv::restore(prompt = FALSE)
+# Note: if not in RStudio, will need to install devtools if not in lock file
+devtools::install(packageFolder, quick = TRUE, dependencies = FALSE)
 
-# Run the package --------------------------------------------------------------
-library(pleTestPackage)
-options(andromedaTempFolder = "s:/andromedaTemp")
-maxCores <- parallel::detectCores()
-outputFolder <- "s:/pleTestPackage"
-connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "pdw",
-                                                                server = Sys.getenv("PDW_SERVER"),
-                                                                user = NULL,
-                                                                password = NULL,
-                                                                port = Sys.getenv("PDW_PORT"))
-cdmDatabaseSchema <- "CDM_IBM_MDCD_V1153.dbo"
-cohortDatabaseSchema <- "scratch.dbo"
-cohortTable <- "mschuemi_skeleton"
-databaseId <- "Synpuf"
-databaseName <- "Medicare Claims Synthetic Public Use Files (SynPUFs)"
-databaseDescription <- "Medicare Claims Synthetic Public Use Files (SynPUFs) were created to allow interested parties to gain familiarity using Medicare claims data while protecting beneficiary privacy. These files are intended to promote development of software and applications that utilize files in this format, train researchers on the use and complexities of Centers for Medicare and Medicaid Services (CMS) claims, and support safe data mining innovations. The SynPUFs were created by combining randomized information from multiple unique beneficiaries and changing variable values. This randomization and combining of beneficiary information ensures privacy of health information."
-oracleTempSchema <- NULL
+# Run the package ------------------------------------------------------------
 
-unlink(outputFolder, recursive = TRUE)
+newSession <- ParallelLogger::makeCluster(numberOfThreads = 1, singleThreadToMain = FALSE)
 
-execute(connectionDetails = connectionDetails,
-        cdmDatabaseSchema = cdmDatabaseSchema,
-        cohortDatabaseSchema = cohortDatabaseSchema,
-        cohortTable = cohortTable,
-        oracleTempSchema = oracleTempSchema,
-        outputFolder = outputFolder,
-        databaseId = databaseId,
-        databaseName = databaseName,
-        databaseDescription = databaseDescription,
-        createCohorts = TRUE,
-        synthesizePositiveControls = TRUE,
-        runAnalyses = TRUE,
-        packageResults = TRUE,
-        maxCores = maxCores)
-
-resultsZipFile <- file.path(outputFolder, "export", paste0("Results_", databaseId, ".zip"))
-dataFolder <- file.path(outputFolder, "shinyData")
-
-prepareForEvidenceExplorer(resultsZipFile = resultsZipFile, dataFolder = dataFolder)
-
-launchEvidenceExplorer(dataFolder = dataFolder, blind = TRUE, launch.browser = FALSE)
-
-launchEvidenceExplorer(dataFolder = dataFolder, blind = FALSE, launch.browser = FALSE)
+runStudy <- function(packageFolder) {
+        setwd(packageFolder)
+        library(pleTestPackage)
+        options(andromedaTempFolder = "s:/andromedaTemp")
+        
+        maxCores <- parallel::detectCores()
+        outputFolder <- "s:/pleTestPackage"
+        unlink(outputFolder, recursive = TRUE)
+        connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "pdw",
+                                                                        server = Sys.getenv("PDW_SERVER"),
+                                                                        user = NULL,
+                                                                        password = NULL,
+                                                                        port = Sys.getenv("PDW_PORT"))
+        cdmDatabaseSchema <- "CDM_IBM_MDCD_V1153.dbo"
+        cohortDatabaseSchema <- "scratch.dbo"
+        cohortTable <- "mschuemi_skeleton"
+        options(sqlRenderTempEmulationSchema = NULL)
+        databaseId <- "Synpuf"
+        databaseName <- "Medicare Claims Synthetic Public Use Files (SynPUFs)"
+        databaseDescription <- "Medicare Claims Synthetic Public Use Files (SynPUFs) were created to allow interested parties to gain familiarity using Medicare claims data while protecting beneficiary privacy. These files are intended to promote development of software and applications that utilize files in this format, train researchers on the use and complexities of Centers for Medicare and Medicaid Services (CMS) claims, and support safe data mining innovations. The SynPUFs were created by combining randomized information from multiple unique beneficiaries and changing variable values. This randomization and combining of beneficiary information ensures privacy of health information."
+        
+        unlink(outputFolder, recursive = TRUE)
+        
+        execute(connectionDetails = connectionDetails,
+                cdmDatabaseSchema = cdmDatabaseSchema,
+                cohortDatabaseSchema = cohortDatabaseSchema,
+                cohortTable = cohortTable,
+                outputFolder = outputFolder,
+                databaseId = databaseId,
+                databaseName = databaseName,
+                databaseDescription = databaseDescription,
+                createCohorts = TRUE,
+                synthesizePositiveControls = TRUE,
+                runAnalyses = TRUE,
+                packageResults = TRUE,
+                maxCores = maxCores)
+}
+ParallelLogger::clusterApply(cluster = newSession, fun = runStudy, x = packageFolder)
+ParallelLogger::stopCluster(newSession)
+# 
+# resultsZipFile <- file.path(outputFolder, "export", paste0("Results_", databaseId, ".zip"))
+# dataFolder <- file.path(outputFolder, "shinyData")
+# 
+# prepareForEvidenceExplorer(resultsZipFile = resultsZipFile, dataFolder = dataFolder)
+# 
+# launchEvidenceExplorer(dataFolder = dataFolder, blind = TRUE, launch.browser = FALSE)
+# 
+# launchEvidenceExplorer(dataFolder = dataFolder, blind = FALSE, launch.browser = FALSE)
